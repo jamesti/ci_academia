db98edd81556583cba77facd2252f0c490d12fc2
James Moreira de Sousa Junior <jamesmsjr@gmail.com>
2014-03-27 17:57:29 -0300

    JQuery DatePicker
    Inclusão do DatePicker

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,5 +4,7 @@
     <script src="<?php echo base_url() ?>assets/js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+    <!-- DatePicker -->
+    <script src="<?php echo base_url() ?>assets/js/jquery.ui.datepicker.min.js"></script>
   </body>
 </html>
