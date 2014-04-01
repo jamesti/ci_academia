e90418eab18c889662938415fe2f2471854c6e70
James Moreira de Sousa Junior <jamesmsjr@gmail.com>
2014-04-01 17:30:13 -0300

    Grunt mincss, concat e uglify
    Minificação de Javascript, css e Concatenação.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,18 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-	  pkg: grunt.file.readJSON('package.json'),
-	  concat: {
+	  
+        pkg: grunt.file.readJSON('package.json'),
+	
+        uglify: {
+                my_target: {
+                  files: {
+                    'assets/js/zcustom.min.js': ['assets/js/custom.js']
+                  }
+                }
+              },
+              
+        concat: {
 		options: {
 		  stripBanners: true,
 		  banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
@@ -18,14 +28,27 @@ module.exports = function(grunt) {
 		  dest: 'assets/dist/built.min.css',
 		},
 	  },
+          
+        cssmin: {
+                minify: {
+                  expand: true,
+                  cwd: 'assets/css/',
+                  src: ['zcustom.css'],
+                  dest: 'assets/css/',
+                  ext: '.min.css'
+                }
+              }
+        
 	});
 
-	// Load the plugin that provides the "concat" task. 
-	grunt.loadNpmTasks('grunt-contrib-concat');	
-	// Load the plugin that provides the "uglify" task.
+        // Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-
+	// Load the plugin that provides the "concat" task. 
+	grunt.loadNpmTasks('grunt-contrib-concat');
+        
+        grunt.loadNpmTasks('grunt-contrib-cssmin');
+        
 	// Default task(s).
-	grunt.registerTask('default', ['concat','uglify']);
+	grunt.registerTask('default', ['uglify','cssmin','concat']);
 
 };
